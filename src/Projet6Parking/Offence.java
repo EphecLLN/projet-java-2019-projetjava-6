1e6f5f1a08f2a77732c1a480dd28ce0e63dbb778
GGEphec <he201732@students.ephec.be>
2019-11-11 10:15:57 +0100

    offence

--- src/Projet6Parking/Offence.java
+++ src/Projet6Parking/Offence.java
@@ -1,14 +1,134 @@
-/**
+/** Ceci est la classe Offence qui représente une infraction dans l'utilisation des parkings.
+ * Chaque Offence se voit attribué un numéro unique afin de pouvoir identifier facilement les objets
+ * Une Offence concerne 2 users, celui en tort et celui qui signale. Un commentaire de max 250c permet de décrire le délit
+ * Une Offence se référe à une Place et donc à un Parking, ainsi qu'une date
  * 
+ * 
+ * @author gaeta_2b6psqs
+ * @date 11-11-19
  */
+
 package Projet6Parking;
 
-/**
- * @author he201732
- *
- */
-public class Offence {
+import java.util.Date;
 
+
+public class Offence {
+	int idOffence; //géré par db
+	User userSignal;
+	User userFlagged;
+	String comment;
+	Place place;
+	Date date;
+	
+	
+	/**
+	 * Ceci est le constructeur d'un objet Offence
+	 * 
+	 * @param signal le User qui signale l'infraction
+	 * @param flag le User qui a comis l'infraction
+	 * @param com le commentaire sur l'infraction
+	 * @param p la Place de Parking où à eu lieue l'infraction
+	 * @param d la Date à laquelle l'infraction à eu lieue
+	 */
+	public Offence(User signal, User flag, String com, Place p, Date d) {
+		userSignal = signal;
+		userFlagged =flag;
+		comment = com;
+		place = p;
+		date = d;
+	}
+	
+
+	
+	/**
+	 * @return the idOffence
+	 */
+	public int getIdOffence() {
+		return idOffence;
+	}
+
+	/**
+	 * @param idOffence the idOffence to set
+	 */
+	public void setIdOffence(int idOffence) {
+		this.idOffence = idOffence;
+	}
+
+	/**
+	 * @return the userSignal
+	 */
+	public User getUserSignal() {
+		return userSignal;
+	}
+
+	/**
+	 * @param userSignal the userSignal to set
+	 */
+	public void setUserSignal(User userSignal) {
+		this.userSignal = userSignal;
+	}
+
+	/**
+	 * @return the userFlagged
+	 */
+	public User getUserFlagged() {
+		return userFlagged;
+	}
+
+	/**
+	 * @param userFlagged the userFlagged to set
+	 */
+	public void setUserFlagged(User userFlagged) {
+		this.userFlagged = userFlagged;
+	}
+
+	/**
+	 * @return the comment
+	 */
+	public String getComment() {
+		return comment;
+	}
+
+	/**
+	 * @param comment the comment to set
+	 */
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	/**
+	 * @return the place
+	 */
+	public Place getPlace() {
+		return place;
+	}
+
+	/**
+	 * @param place the place to set
+	 */
+	public void setPlace(Place place) {
+		this.place = place;
+	}
+
+	/**
+	 * @return the date
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * @param date the date to set
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	
+	
+	
+	
 	/**
 	 * @param args
 	 */
