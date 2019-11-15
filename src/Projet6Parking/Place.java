e9061b6a9f9f088b424de3fb5fdbf9501c20b34f
NathDL <he201742@students.ephec.be>
2019-11-15 12:47:32 +0100

    Place Structure de Base

--- src/Projet6Parking/Place.java
+++ src/Projet6Parking/Place.java
@@ -11,7 +11,88 @@ package Projet6Parking;
  *
  */
 public class Place {
+	
+	//Variables d'instance
+	
+	private int dbId;			//Numero global de la place dans la DB
+	private int number;			//Numero de la place dans le parking
+	private int parkingId;		//Numero du parking
+	private boolean booked;		//Etat de la reservation de la place 
+	
+	
+	//Constructeur(s)
+	
+	public Place(int dbId, int number, int parkingId) {
+		this.dbId = dbId;
+		this.number = number;
+		this.parkingId = parkingId;
+		this.booked = false;
+	}
 
+	
+	//Getters & Setters
+	
+	/**
+	 * @return the dbId
+	 */
+	public int getDbId() {
+		return dbId;
+	}
+
+	/**
+	 * @param dbId the dbId to set
+	 */
+	public void setDbId(int dbId) {
+		this.dbId = dbId;
+	}
+
+	/**
+	 * @return the parkingId
+	 */
+	public int getParkingId() {
+		return parkingId;
+	}
+
+	/**
+	 * @param parkingId the parkingId to set
+	 */
+	public void setParkingId(int parkingId) {
+		this.parkingId = parkingId;
+	}
+
+	/**
+	 * @return the booked
+	 */
+	public boolean isBooked() {
+		return booked;
+	}
+
+	/**
+	 * @param booked the booked to set
+	 */
+	public void setBooked(boolean booked) {
+		this.booked = booked;
+	}
+	
+	
+	//Méthodes
+	
+	public boolean isFree() {
+		return booked;
+	}
+	
+	/**
+	 * 
+	 * @param dbId
+	 * @return
+	 */
+	public Place getPlace(int dbId) {
+		// Requete SQL : SELECT "var_instance_place" FROM "Place" WHERE "idPlace" = "dbId"
+		// Traitement des resultats pour creer un objet Place
+		Place r = new Place(dbId, dbId, dbId);		//int venant du traitement fait à la ligne precedente
+		return r;
+	}
+	
 	/**
 	 * @param args
 	 */
