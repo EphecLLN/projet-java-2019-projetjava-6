1b838df760eab71b35b15e438e81269dca5fd528
BrianLuk10 <56024639+BrianLuk10@users.noreply.github.com>
2019-11-15 08:47:04 +0100

    testbrian

--- src/Projet6Parking/Reservation.java
+++ src/Projet6Parking/Reservation.java
@@ -3,11 +3,56 @@
  */
 package Projet6Parking;
 
+//import date de la bdd
+
 /**
  * @author he201732
  *
  */
 public class Reservation {
+	
+	//propriétés
+	private Place place;
+	private User user;
+	private Date dateStart;
+	private Date dateEnd;
+	
+	//getters 
+	public Place getPlace() {
+		return place;
+	}
+	public User getUser() {
+		return user;
+	}
+	public Date getDateStart() {
+		return dateStart;
+	}
+	public Date getDateEnd() {
+		return dateEnd;
+	}
+	
+	//setters
+	public void setPlace(Place place) {
+		this.place = place;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public void setDateStart(Date dateStart) {
+		this.dateStart = dateStart;
+	}
+	public void setDateEnd(Date dateEnd) {
+		this.dateEnd = dateEnd;
+	}
+	
+	//constructeurs
+	
+	public Reservation(Place place, User user, Date dateS, Date dateE) {
+		this.place = place;
+		this.user = user;
+		this.dateStart = dateS;
+		this.dateEnd = dateE;
+	}
 
 	/**
 	 * @param args
