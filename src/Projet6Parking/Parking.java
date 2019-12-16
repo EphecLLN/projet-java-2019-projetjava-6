b4455fc932bdc5b560e2323f9015913e0b078766
GGEphec <he201732@students.ephec.be>
2019-12-16 10:26:14 +0100

    changement accent

--- src/Projet6Parking/Parking.java
+++ src/Projet6Parking/Parking.java
@@ -140,34 +140,7 @@ public class Parking {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// Users
-		User user1 = new User (0, "gGortz", "motdepasse", "Gortz", "Gaetan", "11111111", "g.gortz@students.ephec.be", "1-AAA-111");
-		User user2 = new User (1, "nDL", "nathan", "De Leener", "Nathan", "22222222", "n.deleener@students.ephec.be", "1-BBB-222");
-		User user3 = new User (2, "bLuk", "1234", "Luk", "Brian", "33333333", "b.luk@students.ephec.be", "1-CCC-333");
-		// Parkings
-		Parking p1 = new Parking (0, "Parking 1", "Localisation 1", 100, 100, "Gratuit");
-		Parking p2 = new Parking (1, "Parking 2", "Localisation 2", 25, 25, "Payant");
-		// Places
-		Place pl1 = new Place (0, p1, 1);
-		Place pl2 = new Place (1, p1, 2);
-		Place pl3 = new Place (2, p2, 1);
-		Place pl4 = new Place (3, p2, 2);
-		// Reservations
-		Reservation r1 = new Reservation (0, pl1, user1);
-		Reservation r2 = new Reservation (1, pl2, user2);
-		Reservation r3 = new Reservation (2, pl4, user3);
-		// Offences
-		Date d = new Date();
-		Offence o1 = new Offence (0, user1, user2, "Blabla1", pl1, d);
 		
-		// Tests
-		//System.out.println(user1.getName() + " " + user1.getFirstname() + " " + user1.getMail() + " " + user1.getPhone() + " " + user1.getPlate() + " " + user1.getPenalty());
-		System.out.println(user1);
-		System.out.println(p1.getPlacesDispo());
-		user1.reserve(pl2);
-		System.out.println(p1.getPlacesDispo());
-		user1.liberePlace(pl2);
-		System.out.println(p1.getPlacesDispo());
 	}
 	
 	/**
@@ -176,7 +149,7 @@ public class Parking {
 	 * @return un parking et ses donnï¿½es
 	 */
 	public String toString() {
-		return "Parking Nï¿½ : " + this.idParking + ", nom : " + this.name + " situï¿½ " + this.position + " a " + this.placesDispo +" places dispos sur " + this.placesTot + " et est " + this.type;
+		return "Parking N° : " + this.idParking + ", nom : " + this.name + " situé " + this.position + " a " + this.placesDispo +" places dispos sur " + this.placesTot + " et est " + this.type;
 	}
 	
   
